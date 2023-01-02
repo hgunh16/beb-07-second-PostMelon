2535863536f14ecfacc483badd37c08469d29fad
baekhun2463 <baekhun2463@gmail.com>
2023-01-02 19:16:08 +0900

    헤더 링크 추가

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -2,7 +2,14 @@ import './Header.css';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <div className="header">this is header!!</div>;
+  return <div className="header">
+                  <Link to="/">POST MELON&nbsp;</Link>
+                  <Link to="/login">&nbsp;Login&nbsp;</Link>
+                  <Link to="/market">&nbsp;Market&nbsp;</Link>
+                  <Link to="/mypage">&nbsp;Mypage&nbsp;</Link>
+                  <Link to="/signup">&nbsp;Signup&nbsp;</Link>
+                  <Link to="/post">&nbsp;Post&nbsp;</Link>
+        </div>;
 };
 
 export default Header;
