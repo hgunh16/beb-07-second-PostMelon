6b411708f33f5476ec3214acbca92eaba10155ec
DongMinKwon <wt0630@naver.com>
2023-01-02 15:26:06 +0900

    error handler area add

--- server/src/app.js
+++ server/src/app.js
@@ -12,12 +12,38 @@ const router = require('./routes');
 const app = express();
 app.set('port', process.env.PORT || 4001);
 
+//middleware setting
+app.use(cors());
+app.use(express.json());
+
 //routes
 app.use('/dev', router.devRouter);
 app.use('/user', router.userRouter);
 app.use('/', (req, res) => {
   res.send('hello!');
 });
+
+//error handler
+
+// app.use((req, res, next) => {
+//   res.status(404).send('Not Found!');
+// });
+
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send({
+//     message: 'Internal Server Error',
+//     stacktrace: err.toString(),
+//   });
+// });
+
+//server listen
+
+// if (process.env.NODE_ENV !== "test") {
+//   app.listen(port, () => {
+//     console.log(`[RUN] StatesAirline Server... | http://localhost:${port}`);
+//   });
+// }
 
 app.listen(app.get('port'), () => {
   console.log(`server is listening at PORT : ${app.get('port')}`);
