b331cc1a946db1a7dfba262944f15ce705e814d6
hgunh16 <hgunh16@gmail.com>
2023-01-05 17:07:54 +0900

    https, 개인키 제외

--- server/src/config/pem_config.js
+++ server/src/config/pem_config.js
@@ -3,8 +3,10 @@ const path = require("path");
 // const keys_dir = "./secure"; // 키 파일, 상대경로 수정 필요
 const absolutePath = path.resolve("src/config/secure");
 
+const key = fs.readFileSync(absolutePath + "/key.pem");
 const cert = fs.readFileSync(absolutePath + "/cert.pem");
 
 module.exports.options = {
     cert,
+    key,
 };
